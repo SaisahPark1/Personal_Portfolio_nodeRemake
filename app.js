99363c43952f28f5bcb55508f49caeb499b3271e
SaisahPark1 <jpark277@west-mec.org>
2026-08-25 15:22:57 -0700

    Created code to switch pages

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ app.use(express.static(path.join(__dirname, 'event_loop')))
 
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'pages','index.html'))
+    res.sendFile(path.join(__dirname,'views','home.html'))
 })
-// app.get('/about', (req,res)=>{
-//     res.sendFile(path.join(__dirname,'pages','about.html'))
-// })
+app.get('/about', (req,res)=>{
+    res.sendFile(path.join(__dirname,'views','about.html'))
+})
 // app.get(/.*/, (req, res)=>{
 //     res.sendFile(path.join(__dirname,'pages','error.html'))
 // })   
